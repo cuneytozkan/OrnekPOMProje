06d0e72bfe29d8063ab3104fedc5c694d4a05674
Cuneyt <cnytozkn2014@gmail.com>
2021-06-19 14:39:41 +0300

    yeni proje guncelleme

---
+++ src/test/java/tests/D22_ReusableMethodsWebList.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+import javax.print.attribute.standard.ReferenceUriSchemesSupported;
+import java.util.List;
+
+public class D22_ReusableMethodsWebList {
+    @Test
+    public void test01(){
+        Driver.getDriver().get(ConfigReader.getProperty("amazon_url"));
+        AmazonPage amazonPage = new AmazonPage();
+        List<WebElement> tumDataWebelement = amazonPage.tumDataWebelementList;
+
+        System.out.println(ReusableMethods.getElementsText(tumDataWebelement));
+
+        System.out.println("====================================");
+        System.out.println(ReusableMethods.getElementsText(amazonPage.tumDataWebelementList ));
+
+
+    }
+}
